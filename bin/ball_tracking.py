1f5a0dc2d4bf734b9447d6dca6f1fa4bdc239474
James Bell <jtb2013@gmail.com>
2018-11-09 17:44:03 -0600

    hrm

--- bin/ball_tracking.py
+++ bin/ball_tracking.py
@@ -1,8 +1,10 @@
+#!/usr/bin/env python
+
 # USAGE
 # python ball_tracking.py --video ball_tracking_example.mp4
 # python ball_tracking.py
 
-# Don't forget about republish [in_transport] in:=<in_base_topic> [out_transport] out:=<out_base_topic>
+# Don't forget about republish [in_transport] in:=/pixelink/image [out_transport] out:=/pixelink/image_py
 
 # import the necessary packages
 from collections import deque
@@ -11,21 +13,29 @@ import numpy as np
 import argparse
 import cv2
 import imutils
-import time
 import roslib
 import rospy
+import traceback
+import logging
+import sys
 from sensor_msgs.msg import Image
+from cv_bridge import CvBridge, CvBridgeError
 from pv_estimator.msg import Meas
 
 class frame_grabber:
 	def __init__(self):
 		self.frame = np.zeros((256,256,3), np.uint8)	
 		self.time = rospy.Time()
+		self.bridge = CvBridge()
+		print('Frame Grabber Initialized')
 
   # Callback for when an image is available
 	def imageCallback(self, msg):
-		np_arr = np.fromstring(msg.data, np.uint8)
-		self.frame = cv2.imdecode(np_arr,cv2.IMREAD_COLOR)
+		print('image called back')
+		try:
+			self.frame = self.bridge.imgmsg_to_cv2(msg.data, "bgr8")
+		except Exception as e:
+			logging.error(traceback.format_exc())
 		#destRGB = cv2.cvtColor(srcBGR, cv2.COLOR_BGR2RGB)
 		self.time = msg.header.stamp
 
@@ -43,20 +53,21 @@ def main(args):
 
 	### ROS variables ###
 	fg = frame_grabber()
-	sub = rospy.Subscriber("/pixelink/image",Image,fg.imageCallback,queue_size = 1)
-	pub = rospy.Publisher("/tracker/meas", Meas)
+	sub = rospy.Subscriber("/pixelink/image_py",Image,fg.imageCallback,queue_size = 100)
+	pub = rospy.Publisher("/tracker/meas", Meas, queue_size = 2)
 	
 	tLastImg = rospy.Time()
 	tCurrImg = rospy.Time()
 	rate = rospy.Rate(100)
-			
+	print('ROS Initialized')
 	# keep looping
 	while not rospy.is_shutdown():
 		v = (0,0)
+		tCurrImg = fg.time
 		# check if new image is available
-		if(tCurrImg.toSec() <= tLastImg.toSec()):
+		if(tCurrImg.to_sec() <= tLastImg.to_sec()):
 			rate.sleep()
-			rospy.spinOnce()
+			print('Sleeping, last image taken at: ' + str(tCurrImg.to_sec()))
 			continue
 		
 		# resize the frame, blur it, and convert it to the HSV
@@ -79,7 +90,7 @@ def main(args):
 			cv2.CHAIN_APPROX_SIMPLE)
 		cnts = cnts[0] if imutils.is_cv2() else cnts[1]
 		center = None
-
+		print('Processed frame')
 		# only proceed if at least one contour was found
 		if len(cnts) > 0:
 			# find the largest contour in the mask, then use
@@ -89,8 +100,8 @@ def main(args):
 			((x, y), radius) = cv2.minEnclosingCircle(c)
 			M = cv2.moments(c)
 			center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
-
-			if(tLastImg.toSec()>0):
+			print('Found balloon')
+			if(tLastImg.to_sec()>0):
 				prevGrey = cv2.cvtColor(cv2.GaussianBlur(prevFrame,(11,11),0),cv2.COLOR_BGR2GRAY)
 				pts, st, err = cv2.calcOptFlowPyrLK(prevGrey,grey,c,None)#,**lk_params)
 				good_old = c[st==1]
@@ -106,21 +117,18 @@ def main(args):
 			# 	cv2.circle(frame, center, 5, (0, 0, 255), -1)
 		prevFrame = frame
 		# show the frame to our screen
-		cv2.imshow("Frame", frame)
-		key = cv2.waitKey(1) & 0xFF
 
-		measMsg =Meas()
+		measMsg = Meas()
 		measMsg.r[0] = center[0]
 		measMsg.r[1] = center[1]
 		measMsg.v[0] = v[0]
 		measMsg.v[1] = v[1]
 		tCurr = rospy.Time.now() 
-		measMsg.tStamp = tCurr.toSec()
+		measMsg.tStamp = tCurr.to_sec()
 		pub.publsih(measMsg)
 
 		tLastImg = tCurrImg
 		rate.sleep()
-		rospy.spinOnce()
 
 if __name__ == '__main__':
 	main(sys.argv)
