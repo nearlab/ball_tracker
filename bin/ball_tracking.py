168b235de66c5d0f7b29e2e2b0e8383f3ebd2f7d
James Bell <jtb2013@gmail.com>
2018-11-12 14:43:53 -0600

    Working as expected

--- bin/ball_tracking.py
+++ bin/ball_tracking.py
@@ -23,110 +23,116 @@ from cv_bridge import CvBridge, CvBridgeError
 from pv_estimator.msg import Meas
 
 class frame_grabber:
-	def __init__(self):
-		self.prevFrame = np.zeros((256,256,3),np.uint8)
-		self.tLastImg = rospy.Time()
-		self.bridge = CvBridge()
-		
-	  self.pub = rospy.Publisher("/tracker/meas", Meas, queue_size = 2)
-		print('Frame Grabber Initialized')
+  def __init__(self):
+    self.prevFrame = np.zeros((256,256,3),np.uint8)
+    self.tLastImg = rospy.Time()
+    self.bridge = CvBridge()
+    
+    self.pub = rospy.Publisher("/tracker/meas", Meas, queue_size = 2)
+    print('Frame Grabber Initialized')
 
   # Callback for when an image is available
-	def imageCallback(self, msg):
-		print('image called back')
-		try:
-			frame = self.bridge.imgmsg_to_cv2(msg.data, "bgr8")
-		except Exception as e:
-			logging.error(traceback.format_exc())
-		#destRGB = cv2.cvtColor(srcBGR, cv2.COLOR_BGR2RGB)
-		tCurrImg = msg.header.stamp
+  def imageCallback(self, msg):
+    found = False
+    try:
+      frame = self.bridge.imgmsg_to_cv2(msg, "bgr8")
+    except Exception as e:
+      print('failed to convert ')
+      print(e)
+      logging.error(traceback.format_exc())
+    #destRGB = cv2.cvtColor(srcBGR, cv2.COLOR_BGR2RGB)
+    tCurrImg = msg.header.stamp
 
-		# Setup Image Processing Variables
-		redLower = (150, 100, 100)
-		redUpper = (200, 255, 255)
-		v = (0,0)
-		
-		# Do image processing
-		redLower = (150, 100, 100)
-		redUpper = (200, 255, 255)
-		
-		# resize the frame, blur it, and convert it to the HSV
-		# color space
-		frame = imutils.resize(frame, width=600)
-		blurred = cv2.GaussianBlur(frame, (11, 11), 0)
-		grey = cv2.cvtColor(blurred, cv2.COLOR_BGR2GRAY)
-		hsv = cv2.cvtColor(blurred, cv2.COLOR_BGR2HSV)
+    # Setup Image Processing Variables
+    redLower = (150, 100, 100)
+    redUpper = (200, 255, 255)
+    v = (0,0)
+    
+    # Do image processing
+    redLower = (150, 100, 100)
+    redUpper = (200, 255, 255)
+    
+    # resize the frame, blur it, and convert it to the HSV
+    # color space
+    frame = imutils.resize(frame, width=600)
+    blurred = cv2.GaussianBlur(frame, (11, 11), 0)
+    grey = cv2.cvtColor(blurred, cv2.COLOR_BGR2GRAY)
+    hsv = cv2.cvtColor(blurred, cv2.COLOR_BGR2HSV)
 
-		# construct a mask for the color "red", then perform
-		# a series of dilations and erosions to remove any small
-		# blobs left in the mask
-		mask = cv2.inRange(hsv, redLower, redUpper)
-		mask = cv2.erode(mask, None, iterations=2)
-		mask = cv2.dilate(mask, None, iterations=2)
+    # construct a mask for the color "red", then perform
+    # a series of dilations and erosions to remove any small
+    # blobs left in the mask
+    mask = cv2.inRange(hsv, redLower, redUpper)
+    mask = cv2.erode(mask, None, iterations=2)
+    mask = cv2.dilate(mask, None, iterations=2)
 
-		# find contours in the mask and initialize the current
-		# (x, y) center of the ball
-		cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL,
-			cv2.CHAIN_APPROX_SIMPLE)
-		cnts = cnts[0] if imutils.is_cv2() else cnts[1]
-		center = None
-		print('Processed frame')
-		# only proceed if at least one contour was found
-		if len(cnts) > 0:
-			# find the largest contour in the mask, then use
-			# it to compute the minimum enclosing circle and
-			# centroid
-			c = max(cnts, key=cv2.contourArea)
-			((x, y), radius) = cv2.minEnclosingCircle(c)
-			M = cv2.moments(c)
-			center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
-			print('Found balloon')
-			if(tLastImg.to_sec()>0):
-				prevGrey = cv2.cvtColor(cv2.GaussianBlur(self.prevFrame,(11,11),0),cv2.COLOR_BGR2GRAY)
-				pts, st, err = cv2.calcOptFlowPyrLK(prevGrey,grey,c,None)#,**lk_params)
-				good_old = c[st==1]
-				good_new = pts[st==1]
-				s = np.sum(good_new-good_old,axis=0)/(tCurrImg - self.tLastImg) #This had better not be dividing by zero...
-				v = s/len(good_new[:,1])
-			# # only proceed if the radius meets a minimum size
-			# if radius > 10:
-			# 	# draw the circle and centroid on the frame,
-			# 	# then update the list of tracked points
-			# 	cv2.circle(frame, (int(x), int(y)), int(radius),
-			# 		(0, 255, 255), 2)
-			# 	cv2.circle(frame, center, 5, (0, 0, 255), -1)
-		self.prevFrame = frame
-		# show the frame to our screen
+    # find contours in the mask and initialize the current
+    # (x, y) center of the ball
+    cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL,
+      cv2.CHAIN_APPROX_SIMPLE)
+    cnts = cnts[0] if imutils.is_cv2() else cnts[1]
+    center = None
+    print('Processed frame')
+    # only proceed if at least one contour was found
+    
+    if len(cnts) > 0:
+      # find the largest contour in the mask, then use
+      # it to compute the minimum enclosing circle and
+      # centroid
+      c = max(cnts, key=cv2.contourArea)
+      ((x, y), radius) = cv2.minEnclosingCircle(c)
+      M = cv2.moments(c)
+      center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
+      print('Found balloon')
+      found = True
+      if(tLastImg.to_sec()>0):
+        prevGrey = cv2.cvtColor(cv2.GaussianBlur(self.prevFrame,(11,11),0),cv2.COLOR_BGR2GRAY)
+        pts, st, err = cv2.calcOptFlowPyrLK(prevGrey,grey,c,None)#,**lk_params)
+        good_old = c[st==1]
+        good_new = pts[st==1]
+        s = np.sum(good_new-good_old,axis=0)/(tCurrImg - self.tLastImg) #This had better not be dividing by zero...
+        v = s/len(good_new[:,1])
+        
+      # # only proceed if the radius meets a minimum size
+      # if radius > 10:
+      # 	# draw the circle and centroid on the frame,
+      # 	# then update the list of tracked points
+      # 	cv2.circle(frame, (int(x), int(y)), int(radius),
+      # 		(0, 255, 255), 2)
+      # 	cv2.circle(frame, center, 5, (0, 0, 255), -1)
+    self.prevFrame = frame
+    # show the frame to our screen
 
-		measMsg = Meas()
-		measMsg.r[0] = center[0]
-		measMsg.r[1] = center[1]
-		measMsg.v[0] = v[0]
-		measMsg.v[1] = v[1]
-		tCurr = rospy.Time.now() 
-		measMsg.tStamp = tCurr.to_sec()
-		self.pub.publsih(measMsg)
+    measMsg = Meas()
+    if(found):
+      measMsg.r[0] = center[0]
+      measMsg.r[1] = center[1]
+      measMsg.v[0] = v[0]
+      measMsg.v[1] = v[1]
+      tCurr = rospy.Time.now() 
+      measMsg.tStamp = tCurr.to_sec()
+      self.pub.publsih(measMsg)
 
-		self.tLastImg = tCurrImg
+    self.tLastImg = tCurrImg
 
 def main(args):
-	rospy.init_node('ball_tracker', anonymous=True)	
+  rospy.init_node('ball_tracker', anonymous=True)	
 
-	### Image Processing variable here ###
+  ### Image Processing variable here ###
 
-	prevFrame =  np.zeros((256,256,3), np.uint8)	
-	# lk_params = dict( winSize = (15,15),
-	# 									maxLevel = 2,
-	# 									criteria = (cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 10, .03))
+  prevFrame =  np.zeros((256,256,3), np.uint8)	
+  # lk_params = dict( winSize = (15,15),
+  # 									maxLevel = 2,
+  # 									criteria = (cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 10, .03))
 
 
-	### ROS variables ###
-	fg = frame_grabber()
+  ### ROS variables ###
+  fg = frame_grabber()
 
-	self.sub = rospy.Subscriber("/pixelink/image_py",Image,fg.imageCallback,queue_size = 100)
-	print('ROS Initialized')
-	rospy.spin()
-	
+  sub = rospy.Subscriber("/pixelink/image",Image,fg.imageCallback,queue_size = 100)
+  print('ROS Initialized')
+  rospy.spin()
+  
 
 if __name__ == '__main__':
-	main(sys.argv)
+  main(sys.argv)
